c384abebbab126ebdc285d5c5b3989a5c4c3bc44
guoyangyang1994 <a20131105798@admindeimac-12.local>
2016-05-12 10:37:35 +0800

    Initial Commit

---
+++ jifenqi1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jifenqi1
+//
+//  Created by 20131105798 on 16/5/12.
+//  Copyright © 2016年 20131105798. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
